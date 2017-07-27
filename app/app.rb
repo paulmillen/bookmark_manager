8d2f3014ab84f378ed944600bdbc77fda3d75a48
paulmillen <pwmillen@gmail.com>
2017-07-27 17:48:24 +0100

    Added sign in

--- app/app.rb
+++ app/app.rb
@@ -49,6 +49,21 @@ class BookmarkManager < Sinatra::Base
     erb :'users/new'
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb :'sessions/new'
+    end
+  end
+
   post '/users' do
     @user = User.create(email: params[:email],
                       password: params[:password],
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,15 @@ class User
 
   validates_format_of :email, :as => :email_address
 
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
+
   def password=(password)
     @password=(password)
     self.password_digest = BCrypt::Password.create(password)
